17088af4ba865879e54962866327cbdb2a958b7c
OR_GOLESTANI <orgolestani@gmail.com>
2021-04-12 16:15:06 +0300

    dont even know man

--- Server/index.js
+++ Server/index.js
@@ -1,34 +1,34 @@
-const dotenv= require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const path = require('path')
-// General imports 
+const path = require("path");
+// General imports
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
 const db = require("./DB");
 const app = express();
-const moviesRouter = require('./routes/movie-router')
-
+const moviesRouter = require("./routes/movie-router");
 
 const PORT = process.env.PORT || 8080;
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', (req, res)=>{
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-db.on('error', (err)=>{console.log(err)})
+db.on("error", (err) => {
+  console.log(err);
+});
+
 app.listen(PORT, () => {
   console.log(`MERN app is connected to server on server on port: ${PORT}`);
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 
-
-app.use('/movies', moviesRouter)
-
+app.use("/movies", moviesRouter);
